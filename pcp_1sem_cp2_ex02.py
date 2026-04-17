83d91e38a94e5f34d8d14ce9bf9a29513fc36470
Kauan e Thiago <you@example.com>
2026-04-17 11:17:13 -0300

    Add exercício 2 - Kauan e Thiago

---
+++ pcp_1sem_cp2_ex02.py
@@ -0,0 +1,28 @@
+# Exercício 10:
+lado_1 = float(input("Digite o valor do lado 1: "))
+lado_2 = float(input("Digite o valor do lado 2: "))
+lado_3 = float(input("Digite o valor do lado 3: "))
+
+lados = [lado_1, lado_2, lado_3]
+
+lados.sort(reverse=True)
+print(lados)
+
+A = lados [0]
+B = lados [1]
+C = lados [2]
+
+if A >= B + C:
+    print("NAO FORMA TRIANGULO")
+else:
+    if A **2 == B **2 + C **2:
+        print("TRIANGULO RETANGULO")
+    elif A **2 > B **2 + C **2:
+        print("TRIANGULO OBTUSANGULO")
+    elif (A**2) < (B**2 + C**2):
+        print("TRIANGULO ACUTANGULO")
+
+    if A == B == C:
+        print("TRIANGULO EQUILATERO")
+    elif A == B or B == C or A == C:
+        print("TRIANGULO ISOSCELES")
