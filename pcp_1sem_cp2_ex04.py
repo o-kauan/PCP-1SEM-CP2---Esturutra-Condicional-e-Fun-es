5b4c6637f7fb1eb6717100cc96530d2d0374d8e9
crfvelloso <guilhermevelloso68@gmail.com>
2026-04-17 11:44:08 -0300

    código

---
+++ pcp_1sem_cp2_ex04.py
@@ -0,0 +1,48 @@
+nome = input("Digite seu nome: ")
+print("==================================================")
+print("1-Gerente\n2-Analista\n3-Assistente\n4-Estagiario")
+print("==================================================")
+cargo = int(input("INSIRA O NUMERO CORRESPONDENTE\nDigite seu cargo: "))
+sal = float(input("Digite o seu salario: "))
+extra = int(input("Digite as horas extras trabalhadas: "))
+falta = int(input("Digite a quantidade de faltas (Periodo de 1 mes): "))
+bonus = input("Recebeu bonus por desempenho? (s / n): ")
+
+def horasextras():
+    valextra = sal * 0.015
+    salcextra = (valextra * extra) + sal
+
+def descfaltas():
+    desc = sal * 0.02
+    descfinal = ((desc * falta) - sal) * -1
+
+def calcbonus():
+    if cargo == 1 and bonus == "s":
+        bonss = 1000
+        bsal = sal + bonss
+    elif cargo == 2 and bonus == "s":
+        bonss = 500
+        bsal = sal + bonss
+    elif cargo == 3 and bonus == "s":
+        bonss = 300
+        bsal = sal + bonss
+    elif cargo == 4 and bonus == "s":
+        bonss = 100
+        bsal = sal + bonss
+    elif bonus == "n":
+        bonss = 0
+    else:
+        print("Erro! Tente novamente!")
+
+total_extra = horasextras()
+total_desc = descfaltas()
+total_bonus = calbonus()
+
+btotal = valextra + bonss
+salariofinal = sal + btotal - desc
+print("=============================================")
+print(f"salario bruto: R${sal}")
+print(f"Valor da hora extra + bonus: R${btotal}")
+print(f"Total de descontos: -R${desc}")
+print(f"Salario final: R${salariofinal}")
+print("=============================================")
